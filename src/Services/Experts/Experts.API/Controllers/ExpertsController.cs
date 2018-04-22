d323be4e2d404414c7fb41e3b7f5e2c0ec4c2527
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2018-04-22 10:57:05 -0500

    Experts controller and model

---
+++ src/Services/Experts/Experts.API/Controllers/ExpertsController.cs
@@ -0,0 +1,40 @@
+using Experts.API.Models;
+
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Experts.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    public class ExpertsController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("/{id:int}")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(Expert),(int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
+
+            var item = new Expert{
+                Id = 1,
+                Name = "Brandon Barnett"
+            };
+
+            if (item != null)
+            {
+                return Ok(item);
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ src/Services/Experts/Experts.API/Models/Expert.cs
@@ -0,0 +1,9 @@
+namespace Experts.API.Models
+{
+    public class Expert
+    {
+        public int Id {get;set;}
+
+        public string Name {get;set;}
+    }
+}
